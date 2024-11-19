da00dad74c3b4dac5692af46b5ea9e5f5fba0b74
Rayhan Afifi <rayhanafifi12@gmail.com>
2024-11-19 12:56:00 +0700

    11111

--- uts-framework/resources/views/welcome.blade.php
+++ uts-framework/resources/views/welcome.blade.php
@@ -33,7 +33,7 @@
 
 <body>
     <div class="container text-center my-5">
-        <h1 class="mb-4">Belajar Controller & View pada Laravel 2 nih</h1>
+        <h1 class="mb-4">Belajar Controller & View pada Laravel</h1>
         {{-- Contoh cara mereferensikan gambar di dalam file blade dengan menggunakan pendekatan Vite --}}
         <img class="img-thumbnail" src="{{ Vite::asset('resources/images/laravel.png') }}" alt="image">
 
